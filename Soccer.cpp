33a37c51bcd8220892781c4a75df628b0897d7c8
Leo Wang <77943019+Le0wang06@users.noreply.github.com>
2024-09-23 09:36:31 -0400

    Update Soccer.cpp

--- Soccer.cpp
+++ Soccer.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+// Global variables
+int team_a_goals = 0, team_b_goals = 0; 
+int team_a_shots_on_goal = 0, team_b_shots_on_goal = 0;
+int team_a_shots_off_goal = 0, team_b_shots_off_goal = 0;
+int team_a_blocked_shots = 0, team_b_blocked_shots = 0;
+int team_a_fouls = 0, team_b_fouls = 0;
+int team_a_yellow_cards = 0, team_b_yellow_cards = 0;
+int team_a_red_cards = 0, team_b_red_cards = 0;
 
